a4aed8c7cb58c3642c41c0535c6295cf93060cd2
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2023-02-06 11:28:56 +0200

    1.0.9 Machine Learning Added

---
+++ machineLearning.js
@@ -0,0 +1,389 @@
+'use-strict'
+
+document.querySelector('#btnMachineLearning').addEventListener('click', function(){
+    clearUI()
+
+    let machineLearning0  = document.createElement('button')
+    let machineLearning1  = document.createElement('button')
+    let machineLearning2  = document.createElement('button')
+    let machineLearning3  = document.createElement('button')
+    let machineLearning4  = document.createElement('button')
+    let machineLearning5  = document.createElement('button')
+    let machineLearning6  = document.createElement('button')
+    let machineLearning7  = document.createElement('button')
+    let machineLearning8  = document.createElement('button')
+    let machineLearning9  = document.createElement('button')
+    let machineLearning10  = document.createElement('button')
+    let machineLearning11  = document.createElement('button')
+    let machineLearning12  = document.createElement('button')
+    let machineLearning13  = document.createElement('button')
+    let machineLearning14  = document.createElement('button')
+    let machineLearning15  = document.createElement('button')
+    let machineLearning16  = document.createElement('button')
+    let machineLearning17  = document.createElement('button')
+    let machineLearning18  = document.createElement('button')
+    let machineLearning19  = document.createElement('button')
+    let machineLearning20  = document.createElement('button')
+    let machineLearning21  = document.createElement('button')
+
+ 
+
+
+
+
+    
+    machineLearning0.textContent = arr[28]['list'][0]['name']
+    machineLearning0.setAttribute("id", "btnMachineLearning0");
+    machineLearning0.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning0)
+    
+    // document.querySelector('#btnMachineLearning0').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning1.textContent = arr[28]['list'][1]['name']
+    machineLearning1.setAttribute("id", "btnMachineLearning1");
+    machineLearning1.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning1)
+
+    // document.querySelector('#btnMachineLearning1').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = `https://api.clarifai.com`
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning2.textContent = arr[28]['list'][2]['name']
+    machineLearning2.setAttribute("id", "btnMachineLearning2");
+    machineLearning2.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning2)
+
+    // document.querySelector('#btnMachineLearning2').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning3.textContent = arr[28]['list'][3]['name']
+    machineLearning3.setAttribute("id", "btnMachineLearning3");
+    machineLearning3.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning3)
+
+    // document.querySelector('#btnMachineLearning3').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning4.textContent = arr[28]['list'][4]['name']
+    machineLearning4.setAttribute("id", "btnMachineLearning4");
+    machineLearning4.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning4)
+
+    // document.querySelector('#btnMachineLearning4').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning5.textContent = arr[28]['list'][5]['name']
+    machineLearning5.setAttribute("id", "btnMachineLearning5");
+    machineLearning5.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning5)
+
+    // document.querySelector('#btnMachineLearning5').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning6.textContent = arr[28]['list'][6]['name']
+    machineLearning6.setAttribute("id", "btnMachineLearning6");
+    machineLearning6.setAttribute("class", "red");
+    resultsContainer.append(machineLearning6)
+
+    // document.querySelector('#btnMachineLearning6').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning7.textContent = arr[28]['list'][7]['name']
+    machineLearning7.setAttribute("id", "btnMachineLearning7");
+    machineLearning7.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning7)
+
+    // document.querySelector('#btnMachineLearning7').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning8.textContent = arr[28]['list'][8]['name']
+    machineLearning8.setAttribute("id", "btnMachineLearning8");
+    machineLearning8.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning8)
+
+    // document.querySelector('#btnMachineLearning8').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning9.textContent = arr[28]['list'][9]['name']
+    machineLearning9.setAttribute("id", "btnMachineLearning9");
+    machineLearning9.setAttribute("class", "red");
+    resultsContainer.append(machineLearning9)
+
+    // document.querySelector('#btnMachineLearning9').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning10.textContent = arr[28]['list'][10]['name']
+    machineLearning10.setAttribute("id", "btnMachineLearning10");
+    machineLearning10.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning10)
+
+    // document.querySelector('#btnMachineLearning10').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = `https://api.irisnet.de/v2/config/`
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning11.textContent = arr[28]['list'][11]['name']
+    machineLearning11.setAttribute("id", "btnMachineLearning11");
+    machineLearning11.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning11)
+
+    // document.querySelector('#btnMachineLearning11').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning12.textContent = arr[28]['list'][12]['name']
+    machineLearning12.setAttribute("id", "btnMachineLearning12");
+    machineLearning12.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning12)
+
+    // document.querySelector('#btnMachineLearning12').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning13.textContent = arr[28]['list'][13]['name']
+    machineLearning13.setAttribute("id", "btnMachineLearning13");
+    machineLearning13.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning13)
+
+    // document.querySelector('#btnMachineLearning13').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning14.textContent = arr[28]['list'][14]['name']
+    machineLearning14.setAttribute("id", "btnMachineLearning14");
+    machineLearning14.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning14)
+
+    // document.querySelector('#btnMachineLearning14').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = `https://api.nlpcloud.io/v1/bart-large-mnli-yahoo-answers/classification`
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning15.textContent = arr[28]['list'][15]['name']
+    machineLearning15.setAttribute("id", "btnMachineLearning15");
+    machineLearning15.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning15)
+
+    // document.querySelector('#btnMachineLearning15').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = `https://api.openvisionapi.com/api/v1/detection`
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning16.textContent = arr[28]['list'][16]['name']
+    machineLearning16.setAttribute("id", "btnMachineLearning16");
+    machineLearning16.setAttribute("class", "green");
+    resultsContainer.append(machineLearning16)
+
+    document.querySelector('#btnMachineLearning16').addEventListener('click',function(){
+    function fetchAPI(){
+        let url = `https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1`
+        fetch(url)
+            .then((response) => response.json())
+            .then((data)=> console.log(data)) 
+            // .then((data)=> renderAPI4(data) )       
+    }
+    fetchAPI()
+    })
+
+    machineLearning17.textContent = arr[28]['list'][17]['name']
+    machineLearning17.setAttribute("id", "btnMachineLearning17");
+    machineLearning17.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning17)
+
+    // document.querySelector('#btnMachineLearning17').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning18.textContent = arr[28]['list'][18]['name']
+    machineLearning18.setAttribute("id", "btnMachineLearning18");
+    machineLearning18.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning18)
+
+    // document.querySelector('#btnMachineLearning18').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning19.textContent = arr[28]['list'][19]['name']
+    machineLearning19.setAttribute("id", "btnMachineLearning19");
+    machineLearning19.setAttribute("class", "red");
+    resultsContainer.append(machineLearning19)
+
+    // document.querySelector('#btnMachineLearning19').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning20.textContent = arr[28]['list'][20]['name']
+    machineLearning20.setAttribute("id", "btnMachineLearning20");
+    machineLearning20.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning20)
+
+    // document.querySelector('#btnMachineLearning20').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+    machineLearning21.textContent = arr[28]['list'][21]['name']
+    machineLearning21.setAttribute("id", "btnMachineLearning21");
+    machineLearning21.setAttribute("class", "yellow");
+    resultsContainer.append(machineLearning21)
+
+    // document.querySelector('#btnMachineLearning21').addEventListener('click',function(){
+    // function fetchAPI(){
+    //     let url = ``
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data)=> console.log(data)) 
+    //         // .then((data)=> renderAPI4(data) )       
+    // }
+    // fetchAPI()
+    // })
+
+})
+
+  
